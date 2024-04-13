3673fc8f509913594d2b2de947053f12763b7c07
effuone <albkfil@gmail.com>
2024-04-14 04:36:49 +0500

    parsed data abstracted and seeded

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
 const prisma = new PrismaClient();
 
 const cardTypes = [
-  { bank: 'BCC', cardTypes: ['#картакарта', '#ironcard', 'juniorcard'] },
+  { bank: 'BCC', cardTypes: ['#картакарта', '#ironcard', '#juniorcard'] },
   {
     bank: 'Halyk',
     cardTypes: [
@@ -19,6 +21,31 @@ const cardTypes = [
     cardTypes: ['Travel', 'Blue', 'Black', 'Solo', 'Детская карта Forte'],
   },
 ];
+
+const categories = [
+  'Здоровье и красота',
+  'Питание и рестораны',
+  'Мода и одежда',
+  'Мебель и домашний декор',
+  'Услуги и разное',
+  'Продукты питания и рынки',
+  'Аксессуары и ювелирные изделия',
+  'Спорт и туристическое снаряжение',
+  'Товары для детей и игрушки',
+  'Путешествия и размещение',
+  'Строительные материалы',
+  'Товары премиум-класса',
+  'Электроника',
+];
+
+const filePath = path.join(__dirname, './data/seed.json');
+
+let partnersAndOffers = [];
+
+fs.readFile(filePath, { encoding: 'utf-8' }).then((json) => {
+  const seedData = JSON.parse(json);
+  partnersAndOffers = seedData;
+});
 
 async function main() {
   for (const entry of cardTypes) {
@@ -36,6 +63,45 @@ async function main() {
         },
       });
     }
+  }
+  // Create categories
+  for (const category of categories) {
+    await prisma.categories.create({
+      data: {
+        name: category,
+      },
+    });
+  }
+
+  for (const item of partnersAndOffers) {
+    const category = await prisma.categories.findFirst({
+      where: { name: item.category },
+    });
+    if (!category) {
+      continue;
+    }
+    const cardType = await prisma.bankCardType.findFirst({
+      where: { name: item.cardPartner },
+    });
+    if (!cardType) {
+      continue;
+    }
+    const partner = await prisma.partner.create({
+      data: {
+        name: item.partner,
+        description: item.description,
+        categoryId: category.id,
+      },
+    });
+
+    await prisma.cashbackOffer.create({
+      data: {
+        partnerId: partner.id,
+        cardTypeId: cardType.id,
+        cashbackPercent: item.cashback,
+        discountPercent: item.discountPercent,
+      },
+    });
   }
 }
 

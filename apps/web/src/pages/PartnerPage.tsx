135cdd185b43186a7bc030b5c536f832c008631e
Sanzhar <sanxzhar@gmail.com>
2024-04-14 11:06:48 +0500

    merge

--- apps/web/src/pages/PartnerPage.tsx
+++ apps/web/src/pages/PartnerPage.tsx
@@ -5,49 +5,73 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel";
 import { Input } from "@/components/ui/input";
-import { FC } from "react";
+import backendApiInstance from "@/services";
+import { FC, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const mockData = {
-  partnerName: "H&M, Мангилик Ел 252/1",
-  category: "Одежда",
-  cards: [
-    {
-      id: 1,
-      bankName: "Halyk Bank",
-      color: "halyk",
-      cardName: "default",
-      isVisa: false,
-      cashback: 10,
-      day: "Выходной День",
-      sum: 5000,
-      best: true,
-    },
-    {
-      id: 2,
-      bankName: "Bank CreditCredit",
-      color: "bcc",
-      cardName: "default",
-      isVisa: true,
-      cashback: 10,
-      day: "Выходной День",
-      sum: 5000,
-      best: false,
-    },
-    {
-      id: 3,
-      bankName: "Forte Bank",
-      color: "forte",
-      cardName: "default",
-      isVisa: true,
-      cashback: 10,
-      day: "Выходной День",
-      sum: 5000,
-      best: false,
-    },
-  ],
-};
+const cardColors = {
+  "Halyk Bonus Digital": "halyk",
+  "Halyk Bonus": "halyk",
+  "Halyk Sinooil Digital": "halyk",
+  "Halyk Black": "halyk",
+  "Halyk Diamond": "halyk",
+  "#картакарта": "bcc",
+  "#ironcard": "bcc",
+  "#juniorcard": "bcc",
+  "Forte Travel": "forte",
+  "Forte Blue": "forte",
+  "Forte Black": "forte",
+  "Forte Solo": "forte",
+  "Детская карта Forte": "forte",
+  "Kaspi Gold": "forte",
+  "Jusan Silver": "bcc",
+}
 
-const Header: FC = () => {
+// const mockData = {
+//   partnerName: "H&M, Мангилик Ел 252/1",
+//   category: "Одежда",
+//   cards: [
+//     {
+//       id: 1,
+//       bankName: "Halyk Bank",
+//       color: "halyk",
+//       cardName: "default",
+//       isVisa: false,
+//       cashback: 10,
+//       day: "Выходной День",
+//       sum: 5000,
+//       best: true,
+//     },
+//     {
+//       id: 2,
+//       bankName: "Bank CreditCredit",
+//       color: "bcc",
+//       cardName: "default",
+//       isVisa: true,
+//       cashback: 10,
+//       day: "Выходной День",
+//       sum: 5000,
+//       best: false,
+//     },
+//     {
+//       id: 3,
+//       bankName: "Forte Bank",
+//       color: "forte",
+//       cardName: "default",
+//       isVisa: true,
+//       cashback: 10,
+//       day: "Выходной День",
+//       sum: 5000,
+//       best: false,
+//     },
+//   ],
+// };
+
+type HeaderProps = {
+  title: string
+}
+
+const Header: FC<HeaderProps> = ({title}) => {
   return (
     <div className="flex flex-col justify-center text-white bg-white">
       <div className="flex flex-col items-center px-8 pt-8 pb-12 w-full bg-neutral-800 max-md:px-5 max-md:max-w-full">
@@ -63,7 +87,7 @@ const Header: FC = () => {
           <Input
             id="search-field"
             className="md:mt-0 mb-8 md:mb-9 md:my-0 text-white bg-neutral-800 flex z-10 gap-5 justify-between self-center px-5 max-w-full text-base font-medium rounded-xl border border-solid shadow-sm border-zinc-300 w-[300px] max-md:flex-wrap max-md:pl-5"
-            value="H&M, Мангилик Ел 252/1"
+            value={title}
             type="search"
             name="search"
             placeholder="Поиск"
@@ -80,89 +104,170 @@ const Header: FC = () => {
 
 interface CardData {
   id: number;
-  bankName: string;
-  color: string;
-  cardName: string;
-  isVisa: boolean;
-  cashback: number;
-  day: string;
-  sum: number;
-  best: boolean;
+  name: string;
+  image: string | null;
+  address: string | null;
+  description: string | null;
+  Category: {
+    id: 1,
+    name: string
+  };
+  CashbackOffers: {
+    id: number,
+    partnerId: number,
+    cardTypeId: number,
+    cashbackPercent: number,
+    discountPercent: number,
+    requirements: string | null,
+    offerEndDate: string | null,
+    BankCardType: {
+      id: number,
+      name: string,
+      backId: number
+    }
+  }[]
 }
 
 interface CardComponentProps {
-  cardData: CardData; // define the type for card data
+  bestOptionId: number,
+  cardInfo: {
+    id: number,
+      partnerId: number,
+      cardTypeId: number,
+      cashbackPercent: number,
+      discountPercent: number,
+      requirements: string | null,
+      offerEndDate: string | null,
+      BankCardType: {
+        id: number,
+        name: string,
+        backId: number
+      } // define the type for card data
+  }
 }
 
-const CardComponent: FC<CardComponentProps> = ({ cardData }) => {
+const CardComponent: FC<CardComponentProps> = ({ bestOptionId, cardInfo }) => {
   return (
     <>
       <div
-        className={`relative flex flex-col px-2 text-base font-medium text-center text-black max-w-[309px] gap-y-3 justify-center p-2 ${cardData.best ? "border-2 border-solid border-[#FF6B00] rounded-[18px]" : ""}`}
+        className={`relative flex flex-col px-2 text-base font-medium text-center text-black max-w-[309px] gap-y-3 justify-center p-2 ${cardInfo.id  == bestOptionId ? "border-2 border-solid border-[#FF6B00] rounded-[18px]" : ""}`}
       >
-        {cardData.best === true && (
+        {cardInfo.id  == bestOptionId && (
           <p className="absolute top-0 left-0 px-2 py-1 text-xs font-bold text-white bg-[#FF6B00] rounded-tl-[18px] rounded-br-[18px]">
             Лучшее предложение
           </p>
         )}
         <CardCustom
-          bankName={cardData.bankName}
-          color={cardData.color}
-          cardName={cardData.cardName}
-          isVisa={cardData.isVisa}
+          bankName={cardInfo.BankCardType.name}
+          color={cardColors[cardInfo.BankCardType.name]}
+          cardName=""
+          isVisa={true}
         />
-        <div className="justify-center items-center px-16 py-[15px] font-bold text-white rounded-xl bg-zinc-800 w-full">
-          Кэшбэк {cardData.cashback}%
-        </div>
-        <div className="justify-center items-center px-16 py-[14px] rounded-xl border border-solid border-stone-300 w-full">
-          {cardData.day}
-        </div>
-        <div className="justify-center items-center px-16 py-[14px] rounded-xl border border-solid border-stone-300 w-full">
-          До {cardData.sum} тенге
-        </div>
+        {cardInfo.cashbackPercent > 0 && 
+          <div className="justify-center items-center px-16 py-[15px] font-bold text-white rounded-xl bg-zinc-800 w-full">
+            Кэшбэк {cardInfo.cashbackPercent}%
+          </div>}
+        {cardInfo.offerEndDate && 
+          <div className="justify-center items-center px-16 py-[14px] rounded-xl border border-solid border-stone-300 w-full">
+            {cardInfo.offerEndDate}
+          </div>
+        }
+        {cardInfo.discountPercent > 0 && 
+          <div className="justify-center items-center px-16 py-[15px] font-bold rounded-xl border border-solid border-stone-300 w-full">
+            Cкидка {cardInfo.cashbackPercent}%
+          </div>
+        }
+        {cardInfo.offerEndDate && 
+          <div className="justify-center items-center px-16 py-[14px] rounded-xl border border-solid border-stone-300 w-full">
+            До {cardInfo.offerEndDate} 
+          </div>
+        }
+        {
+          cardInfo.requirements && 
+          <div className="justify-center items-center px-16 py-[14px] rounded-xl border border-solid border-stone-300 w-full">
+            Условия: {cardInfo.requirements} 
+          </div>
+        }
       </div>
     </>
   );
 };
 
 export const PartnerPage: FC = () => {
+
+  const [PartnerData, setPartnerData] = useState<CardData | null>(null);
+  const [BestOption, setBestOption] = useState<number | null>(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchPartnerData(){
+      try{
+        const response = await backendApiInstance.get(`/cards/partners/${id}`);
+        setPartnerData(response.data); 
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchPartnerData();
+  }, [])
+
+  useEffect(() => {
+    if(PartnerData){
+      let tempValue = PartnerData.CashbackOffers[0]
+      for(const option of PartnerData.CashbackOffers){
+        if(option.cashbackPercent > tempValue.cashbackPercent){
+          tempValue = option
+        }
+      }
+      setBestOption(tempValue.id);
+    }
+  }, [PartnerData])
+
+
   return (
     <>
-      <Header />
-      <div className="flex gap-x-4 justify-center -mt-[24px]">
-        <div className="justify-center px-3 md:px-6 py-[10px] md:py-3.5 text-lg md:text-2xl font-semibold text-center text-black bg-white rounded-xl shadow-md">
-          {mockData.partnerName}
-        </div>
-        <div className="justify-center px-3 md:px-6 py-[10px] md:py-3.5.5 text-lg md:text-2xl font-semibold text-center text-black bg-white rounded-xl shadow-md">
-          {mockData.category}
-        </div>
-      </div>
-      <Carousel
-        opts={{
-          align: "start",
-        }}
-        className="w-full mt-[60px] md:px-28 hidden md:block"
-      >
-        <CarouselContent>
-          {mockData.cards.map((card, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-              <div className="p-1 flex justify-center">
-                <CardComponent cardData={card} />
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
-      <div className="mt-8">
-        {mockData.cards.map((card) => (
-          <div
-            key={card.cardName}
-            className="p-1 flex pb-3 justify-center md:hidden"
-          >
-            <CardComponent cardData={card} />
+      {PartnerData && 
+      <div>
+
+        <Header title={PartnerData.name} />
+
+        <div className="flex gap-x-4 justify-center -mt-[24px]">
+          <div className="justify-center px-3 md:px-6 py-[10px] md:py-3.5 text-lg md:text-2xl font-semibold text-center text-black bg-white rounded-xl shadow-md">
+            {PartnerData.name}
           </div>
-        ))}
+          <div className="justify-center px-3 md:px-6 py-[10px] md:py-3.5.5 text-lg md:text-2xl font-semibold text-center text-black bg-white rounded-xl shadow-md">
+            {PartnerData.Category.name}
+          </div>
+        </div>
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full mt-[60px] md:px-28 hidden md:block"
+        >
+          <CarouselContent>
+            {PartnerData.CashbackOffers.map((card, index) => (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                <div className="p-1 flex justify-center">
+                  <CardComponent bestOptionId={BestOption} cardInfo={card} />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+        <div className="mt-8">
+          {PartnerData.CashbackOffers.map((card) => (
+            <div
+              key={card.id}
+              className="p-1 flex pb-3 justify-center md:hidden"
+            >
+              <CardComponent bestOptionId={BestOption} cardInfo={card} />
+            </div>
+          ))}
+        </div>
       </div>
+      }
     </>
   );
 };

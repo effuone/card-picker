133af1f80cf81ac92e2ecd43d1547596891391c0
Jarasar <49629027+ZhQriks@users.noreply.github.com>
2024-04-14 08:02:35 +0500

    add func (#3)

--- apps/web/src/components/card-custom.tsx
+++ apps/web/src/components/card-custom.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 
 interface CardCustomProps {
   bankName: string;
@@ -14,29 +14,29 @@ export const CardCustom: React.FC<CardCustomProps> = ({
   isVisa,
 }) => {
   const colors = {
-    jusan: 'bg-gradient-to-r from-orange-500 to-orange-400',
-    bcc: 'bg-gradient-to-r from-green-300 to-green-400',
-    halyk: 'bg-gradient-to-r from-emerald-600 to-emerald-600',
-    forte: 'bg-gradient-to-r from-rose-700 to-red-600',
+    jusan: "bg-gradient-to-r from-orange-500 to-orange-400",
+    bcc: "bg-gradient-to-r from-green-300 to-green-400",
+    halyk: "bg-gradient-to-r from-emerald-600 to-emerald-600",
+    forte: "bg-gradient-to-r from-rose-700 to-red-600",
   };
 
   return (
     <div
-      className={`flex flex-col px-5 pt-6 pb-12 rounded-xl ${colors[color]} w-full min-w-[290px] md:w-[300px] !h-full h-[180px]`}
+      className={`flex flex-col px-5 pt-6 md:pt-3 pb-12 md:pb-5 rounded-xl ${colors[color]} w-full md:min-w-[290px] md:max-w-[300px] h-[180px] md:h-[170px]`}
     >
       <div className="flex gap-5 text-2xl font-bold text-white">
-        <div className="flex-auto">{bankName}</div>
+        <div className="flex-auto text-start">{bankName}</div>
         <img
           loading="lazy"
           src={
             isVisa
-              ? 'https://i.imgur.com/q3vT2My.png'
-              : 'https://i.imgur.com/E08Rmgv.png'
+              ? "https://i.imgur.com/T7xKfzy.png"
+              : "https://i.imgur.com/KUdI4R2.png"
           }
           className=" w-[80px] h-[45px] object-contain"
         />
       </div>
-      <div className="mt-2.5 text-xl text-white">{cardName}</div>
+      <div className="mt-2.5 text-xl text-white text-start">{cardName}</div>
       <img
         loading="lazy"
         src="https://i.imgur.com/3ofIl7Q.png"
